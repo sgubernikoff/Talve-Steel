48e96c5bb6d9bf3b227a78f7a833aac2658804e0
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-07-18 12:57:34 -0400

    navbar update

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import mocklogo from "./mocklogo.png";
 
 function Navbar() {
   function myFunction() {
@@ -18,12 +19,23 @@ function Navbar() {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       />
 
+      <img src={mocklogo} />
       <div className="topnav" id="myTopnav">
-        <NavLink to="/">Talve Metals</NavLink>
-        <NavLink to="/about">About Us</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/feature">******</NavLink>
+        <NavLink onClick={myFunction} to="/">
+          Home
+        </NavLink>
+        <NavLink onClick={myFunction} to="/about">
+          About Us
+        </NavLink>
+        <NavLink onClick={myFunction} to="/products">
+          Products
+        </NavLink>
+        <NavLink onClick={myFunction} to="/contact">
+          Contact
+        </NavLink>
+        <NavLink onClick={myFunction} to="/feature">
+          ******
+        </NavLink>
         <a to="javascript:void(0);" className="icon" onClick={myFunction}>
           <i className="fa fa-bars"></i>
         </a>
