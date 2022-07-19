6fb73e4da9fb8f127616e62f3176a054d063631e
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-07-19 16:27:30 -0400

    contact page

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -3,6 +3,7 @@ import React from "react";
 function Contact() {
   return (
     <div className="contacts">
+      <img className="contact_banner" />
       <h1>Contact Page Here</h1>
     </div>
   );
