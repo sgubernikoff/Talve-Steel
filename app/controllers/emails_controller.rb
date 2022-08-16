c0a102a894d5302c1c7e997a9ecbc491a5d1fa53
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-16 00:39:23 -0400

    test for deploy

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,8 @@
 class EmailsController < ApplicationController
+    def index
+        render json:{a:'idk'}
+    end
+    
     def send_email
         SendEmailMailer.with(email:params[:email],first:params[:first],last:params[:last],message:params[:message]).test_email.deliver_now
         SendEmailMailer.with(email:params[:email],first:params[:first],last:params[:last]).thank_you_email.deliver_now
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/send_email', to: 'emails#send_email'
+  get '/emails', to: 'emails#index'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
