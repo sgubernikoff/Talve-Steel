702534c1f849d97b1f09609105ff19106b4f0815
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-16 09:47:50 -0400

    messing with email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['SMTP_USER_NAME'],
-    :password             => ENV['SMTP_PASSWORD2'],
+    :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
