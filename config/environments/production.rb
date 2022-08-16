4fa4e7c5d52d089a4e9c3b90b73a7c9a6e6497fc
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-15 23:37:13 -0400

    updated host url for email in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000' #replace with your own url
+  host = 'http://anchorstainless.herokuapp.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
 # SMTP settings for gmail
