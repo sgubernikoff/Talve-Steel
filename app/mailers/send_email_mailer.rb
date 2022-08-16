48596fc904215e3edc73969c099b1c39a45a7c08
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-15 23:23:16 -0400

    test emails for deployment

--- app/mailers/send_email_mailer.rb
+++ app/mailers/send_email_mailer.rb
@@ -6,7 +6,7 @@ class SendEmailMailer < ApplicationMailer
         @first = params[:first]
         @last = params[:last]
         @body = params[:message]
-        mail(:to=>['larry@ykgrp.com','zack@ykgrp.com'], subject:'New Message')
+        mail(:to=>['sgubernikoff@gmail.com','agubernikoff@gmail.com'], subject:'New Message')
     end
 
     def thank_you_email
