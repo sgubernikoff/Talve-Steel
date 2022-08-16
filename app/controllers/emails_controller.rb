68024005634397e219cf7e90919988aa40ac833e
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-16 10:48:56 -0400

    deployed email works

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,8 +4,8 @@ class EmailsController < ApplicationController
     end
 
     def send_email
-        SendEmailMailer.with(email:params[:email],first:params[:first],last:params[:last],message:params[:message]).test_email.deliver_now
-        SendEmailMailer.with(email:params[:email],first:params[:first],last:params[:last]).thank_you_email.deliver_now
+        SendEmailMailer.with(email:params[:email],first:params[:first],last:params[:last],message:params[:message]).test_email.deliver_later
+        SendEmailMailer.with(email:params[:email],first:params[:first],last:params[:last]).thank_you_email.deliver_later
         render json:{email:params[:email],first:params[:first],last:params[:last],message:params[:message]}
     end
 end
