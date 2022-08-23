b310bf1bbec86dcadb59ef40e17341f01c75a8cd
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-08-23 17:07:42 -0400

    table add

--- client/src/Product.js
+++ client/src/Product.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ProductCard from "./ProductCard";
+// import ProductCard from "./ProductCard";
 
 function Product() {
-  const products = {
-    name: "Product Name",
-    price: "$10.99",
-    image:
-      "https://st3.depositphotos.com/8069272/35760/v/380/depositphotos_357601916-stock-illustration-realistic-paper-box-mockup-packaging.jpg?forcejpeg=true",
-  };
+  // const products = {
+  //   name: "Product Name",
+  //   price: "$10.99",
+  //   image:
+  //     "https://st3.depositphotos.com/8069272/35760/v/380/depositphotos_357601916-stock-illustration-realistic-paper-box-mockup-packaging.jpg?forcejpeg=true",
+  // };
   return (
     <div className="product">
       <div className="new_banner3">
@@ -16,16 +16,56 @@ function Product() {
           <h2 className="mainhead">VIEW INVENTORY</h2>
         </div>
       </div>
-      <div className="product_hold">
-        <ProductCard products={products} />
-        <ProductCard products={products} />
-        <ProductCard products={products} />
-        <ProductCard products={products} />
-        <ProductCard products={products} />
-        <ProductCard products={products} />
-        <ProductCard products={products} />
-        <ProductCard products={products} />
+      <div className="table_product">
+        <table>
+          <tr>
+            <th>No.</th>
+            <th>Grade</th>
+            <th>Commodities & Specification (MM)</th>
+            <th>Net Weight (KGs)</th>
+            <th>Gross Weight (KGs)</th>
+            <th>Quantity</th>
+            <th>Pkgs</th>
+          </tr>
+          <tr>
+            <td>Anom</td>
+            <td>19</td>
+            <td>Male</td>
+            <td>Anom</td>
+            <td>19</td>
+            <td>Male</td>
+            <td>Male</td>
+          </tr>
+          <tr>
+            <td>Megha</td>
+            <td>19</td>
+            <td>Female</td>
+            <td>Anom</td>
+            <td>19</td>
+            <td>Male</td>
+            <td>Male</td>
+          </tr>
+          <tr>
+            <td>Subham</td>
+            <td>25</td>
+            <td>Male</td>
+            <td>Anom</td>
+            <td>19</td>
+            <td>Male</td>
+            <td>Male</td>
+          </tr>
+        </table>
       </div>
+      {/* <div className="product_hold">
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+      </div> */}
     </div>
   );
 }
--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 
-function TeamMemberCard({ products }) {
-  console.log(products.image);
-  return (
-    <div className="product_card">
-      <img className="prod_image" src={products.image}></img>
-      <h3 className="prod_name">{products.name}</h3>
-      <p className="prod_text">{products.price}</p>
-    </div>
-  );
-}
+// function TeamMemberCard({ products }) {
+//   console.log(products.image);
+//   return (
+//     <div className="product_card">
+//       <img className="prod_image" src={products.image}></img>
+//       <h3 className="prod_name">{products.name}</h3>
+//       <p className="prod_text">{products.price}</p>
+//     </div>
+//   );
+// }
 
-export default TeamMemberCard;
+// export default TeamMemberCard;
