5dc5fd6969634978addaf0d366aa8252fbaf0a89
alexander gubernikoff <agubernikoff@gmail.com>
2022-09-01 12:49:19 -0400

    adding copy

--- client/src/About.js
+++ client/src/About.js
@@ -15,7 +15,7 @@ function About() {
     email: "Larry@anchorstainless.com",
     phone: "+1(516)-721-4603",
     blurb:
-      "Larry Talve has been in the stainless steel industry for over 35 years. Prior to Anchor Stainless, Larry was a principal owner of Franklin Stainless, which was founded in 1955 by his father Jack. At Franklin, Larry oversaw all sales and production. He has extensive knowledge in slitting, leveling, and polishing as he ran all warehouse operations during his time at Franklin. Larry grew sales exponentially after taking over the company and sold the business in 2008. Since then, Larry has worked in a variety of industries including real estate and finance. Larry’s professional career has almost entirely been dedicated to the stainless steel industry. His passion and desire for the business fueled his decision to found Anchor Stainless in 2022 along with his son, Zachary.",
+      "Larry Talve has been in the stainless steel industry for over 35 years. Prior to Anchor Stainless, Larry was a principal owner of Franklin Stainless, which was founded in 1955 by his father Jack and uncle Mark. At Franklin, Larry oversaw all sales, production, and warehouse operations. He has extensive knowledge in slitting, leveling, and polishing. Larry grew sales exponentially after taking over the company and sold the business in 2008. Since then, Larry has worked in a variety of industries including real estate and finance. Larry’s professional career has almost entirely been dedicated to the stainless steel industry. His passion and desire for the business fueled his decision to found Anchor Stainless in 2022 along with his son, Zachary.",
   };
   const teamMember = {
     isBoss: false,
--- client/src/SecondaryAbout.js
+++ client/src/SecondaryAbout.js
@@ -4,42 +4,20 @@ import talvesteel from "./talvesteel.jpg";
 function SecondaryAbout() {
   return (
     <div className="sec_about">
-      <h1 className="who">Lorem Ipsum Dolor</h1>
+      <h1 className="who">Who are we?</h1>
       <div className="para_hold">
         <div className="paragraph">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-            feugiat pretium nibh ipsum consequat nisl vel. Non quam lacus
-            suspendisse faucibus interdum posuere lorem ipsum. Purus viverra
-            accumsan in nisl nisi scelerisque eu. Volutpat consequat mauris nunc
-            congue nisi vitae. Tempus egestas sed sed risus pretium quam
-            vulputate dignissim suspendisse. Leo integer malesuada nunc vel
-            risus commodo viverra maecenas accumsan. Adipiscing vitae proin
-            sagittis nisl rhoncus mattis rhoncus. Varius vel pharetra vel turpis
-            nunc. Ultricies mi quis hendrerit dolor magna eget est lorem. Ornare
-            arcu dui vivamus arcu felis. Sit amet luctus venenatis lectus magna
-            fringilla urna porttitor rhoncus. Erat nam at lectus urna duis
-            convallis convallis. Adipiscing tristique risus nec feugiat in.
-            Feugiat in ante metus dictum at. Suspendisse interdum consectetur
-            libero id faucibus nisl tincidunt. Rhoncus dolor purus non enim
-            praesent elementum.
-          </p>
-           {" "}
-          <p>
-            Viverra maecenas accumsan lacus vel facilisis volutpat est velit
-            egestas. Morbi tempus iaculis urna id volutpat lacus laoreet.
-            Porttitor leo a diam sollicitudin. Sed velit dignissim sodales ut eu
-            sem integer vitae. Est velit egestas dui id ornare. Rhoncus mattis
-            rhoncus urna neque viverra justo. Porttitor massa id neque aliquam
-            vestibulum morbi. Et ultrices neque ornare aenean euismod elementum
-            nisi. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar
-            proin. Enim nec dui nunc mattis enim ut tellus. Bibendum enim
-            facilisis gravida neque convallis. Sed enim ut sem viverra aliquet
-            eget sit amet tellus. Eros donec ac odio tempor orci dapibus.
-            Ullamcorper eget nulla facilisi etiam. Quam viverra orci sagittis eu
-            volutpat odio. Malesuada fames ac turpis egestas integer eget
-            aliquet. In nibh mauris cursus mattis molestie a.
+            Anchor Stainless, a stainless steel distribution company, was
+            founded in 2022 with the goal of providing customers throughout the
+            United States with quality material at competitive prices. Anchor
+            Stainless stocks type 304L, 316L, and type 400 stainless with
+            thickness running from .010” to .125”. In addition, we have the
+            capability to provide slitting and polishing services through our
+            domestic and overseas partners. We pride ourselves on providing
+            seamless customer service and holding nothing but exceptional
+            quality products for our customers. Anchor Stainless’s office is
+            based in Manhattan and our warehouse is located in Baltimore.
           </p>
         </div>
       </div>
